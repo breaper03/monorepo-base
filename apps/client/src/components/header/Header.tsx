59f6631e822931e694662e8cd3d4370ad3b4c719
breaper03 <breaper2021@gmail.com>
2023-07-08 12:21:49 -0400

    fix 2

--- apps/client/src/components/header/Header.tsx
+++ apps/client/src/components/header/Header.tsx
@@ -26,11 +26,11 @@ export const Header = () => {
                   {/* Mobile menu button*/}
                   <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
-                    {/* {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    {open ? (
+                      <HiBell className="block h-6 w-6" aria-hidden="true" />
                     ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )} */}
+                      <HiBell className="block h-6 w-6" aria-hidden="true" />
+                    )}
                   </Disclosure.Button>
                 </div>
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
